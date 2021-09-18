a0e41dca41eb51d8bb4ab5f48877b370fbb49865
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-09-18 23:43:56 +0600

    User data loaded form JsonPlacehoder

--- module-45-react/fundamental-concept/src/App.js
+++ module-45-react/fundamental-concept/src/App.js
@@ -1,14 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <LoadUser></LoadUser>
       <Student name='Ahsan Kabir' roll='23' batch='3rd'></Student>
       <Student name='Nasir Hossain' roll='21' batch='3rd'></Student>
       <Student name='Kazi Tajnur Islam' roll='28' batch='4th'></Student>
       <Student name='Riyad Hossain' roll='30' batch='4th'></Student>
+    </div>
+  );
+}
+
+function LoadUser() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(resp => resp.json())
+      .then(data => setUsers(data))
+  }, []);
+
+  return (
+    <div>
+      <h1>User Loaded: {users.length}</h1>
     </div>
   );
 }
