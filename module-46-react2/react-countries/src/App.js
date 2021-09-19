8225a3c94239f4e7176ffd14acf72166caeba20d
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-09-20 00:12:20 +0600

    Country data fatched and showing the fetched result

--- module-46-react2/react-countries/src/App.js
+++ module-46-react2/react-countries/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Country></Country>
+    </div>
+  );
+}
+
+function Country() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(resp => resp.json())
+      .then(data => setCountries(data));
+  }, []);
+  return (
+    <div>
+      <h1>Total Countries: {countries.length}</h1>
+
     </div>
   );
 }
