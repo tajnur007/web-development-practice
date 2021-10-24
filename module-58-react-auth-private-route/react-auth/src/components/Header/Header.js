282db72ebbcb38fef008711b62273238ba6f434a
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-10-24 17:29:04 +0600

    Username and logout button added

--- module-58-react-auth-private-route/react-auth/src/components/Header/Header.js
+++ module-58-react-auth-private-route/react-auth/src/components/Header/Header.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Header.css';
 import { NavLink } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 
 const Header = () => {
+    const { user } = useFirebase();
     return (
         <div className="header">
             <NavLink exact to="/">Home </NavLink>
             <NavLink exact to="/login">Login </NavLink>
             <NavLink exact to="/register">Register </NavLink>
+            <span>{user.displayName} </span>
+            <button>Logout</button>
         </div>
     );
 };
