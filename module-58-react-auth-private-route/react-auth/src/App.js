eeb541a07abc85d1a51e26a1722a685c2cbd4594
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-10-24 10:31:49 +0600

    Routing path added

--- module-58-react-auth-private-route/react-auth/src/App.js
+++ module-58-react-auth-private-route/react-auth/src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route exact to="/">
+            <Home />
+          </Route>
+          <Route exact to="/about">
+            <About />
+          </Route>
+          <Route exact to="/login">
+            <Login />
+          </Route>
+          <Route exact to="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
