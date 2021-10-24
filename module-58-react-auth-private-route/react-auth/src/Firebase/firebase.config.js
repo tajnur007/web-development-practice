13018cc256bcae08ce76f80ecc4d8133e5331835
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-10-24 10:54:31 +0600

    Firebase initialization porcess added

---
+++ module-58-react-auth-private-route/react-auth/src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2Ge1QTtd1UR9SYELuS1UcVKLqjJ-b8IA",
+    authDomain: "simple-firebase-authenti-a519c.firebaseapp.com",
+    projectId: "simple-firebase-authenti-a519c",
+    storageBucket: "simple-firebase-authenti-a519c.appspot.com",
+    messagingSenderId: "835082671137",
+    appId: "1:835082671137:web:2433ce9e86e1af1ea3bf60"
+};
+
+export default firebaseConfig;
