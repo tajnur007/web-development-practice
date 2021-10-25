be44531c9b4b732ed85bbc2d822d6826de617b3a
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-10-26 00:40:19 +0600

    PrivateRoute component complete

---
+++ module-58-react-auth-private-route/react-auth/src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router';
+import useAuth from '../../hooks/useAuth';
+
+const PrivateRoute = ({ children, ...rest }) => {
+    let auth = useAuth();
+    return (
+        <Route
+            {...rest}
+            render={({ location }) =>
+                auth.user ? (
+                    children
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: "/login",
+                            state: { from: location }
+                        }}
+                    />
+                )
+            }
+        />
+    );
+};
+
+export default PrivateRoute;
