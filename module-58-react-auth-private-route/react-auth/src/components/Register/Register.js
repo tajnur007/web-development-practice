23f51b7cc8eaafd6b0e57d71f7b06af096191072
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-10-25 19:07:50 +0600

    Registration process done

--- module-58-react-auth-private-route/react-auth/src/components/Register/Register.js
+++ module-58-react-auth-private-route/react-auth/src/components/Register/Register.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const { createAccount } = useFirebase();
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleRegister = () => {
+        createAccount(email, password);
+    }
+
     return (
         <div>
             <h2>Please create an account</h2>
-            <input type="text" placeholder="What is your email?" />
+            <input onChange={handleEmail} type="text" placeholder="What is your email?" />
             <br />
-            <input type="text" placeholder="Give a password" />
+            <input onChange={handlePassword} type="text" placeholder="Give a password" />
             <br />
-            <button>Register</button>
+            <button onClick={handleRegister}>Register</button>
             <br />
             <Link to="/login">Already have an account?</Link>
         </div>
