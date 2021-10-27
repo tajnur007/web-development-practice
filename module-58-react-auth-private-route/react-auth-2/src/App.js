5acf7ea537021c3adc52ff1b068155fbd83a48b6
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-10-27 13:22:24 +0600

    Basic routes added

--- module-58-react-auth-private-route/react-auth-2/src/App.js
+++ module-58-react-auth-private-route/react-auth-2/src/App.js
@@ -1,9 +1,43 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Album from './components/Album/Album';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Posts from './components/Posts/Posts';
+import Register from './components/Register/Register';
+import UserDetails from './components/UserDetails/UserDetails';
+import Users from './components/Users/Users';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/users">
+            <Users />
+          </Route>
+          <Route exact path="/posts">
+            <Posts />
+          </Route>
+          <Route exact path="/album">
+            <Album />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/userDetails">
+            <UserDetails />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
