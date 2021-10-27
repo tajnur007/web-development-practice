f6831ec70886ff480f6dd6fe47875900271a14b0
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-10-27 13:36:36 +0600

    Simple message showed

--- module-58-react-auth-private-route/react-auth-2/src/components/Home/Home.js
+++ module-58-react-auth-private-route/react-auth-2/src/components/Home/Home.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-
+            <h1>Welcome to Home page!</h1>
+            <h3>This is a demo page and you can visit pages by clicking the nav links.</h3>
         </div>
     );
 };
