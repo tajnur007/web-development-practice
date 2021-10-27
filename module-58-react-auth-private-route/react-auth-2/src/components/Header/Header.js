4fa944733b4baee714906e08648c01d2f4fa510d
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-10-27 13:34:28 +0600

    Nav links added

--- module-58-react-auth-private-route/react-auth-2/src/components/Header/Header.js
+++ module-58-react-auth-private-route/react-auth-2/src/components/Header/Header.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './Header.css';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-
+        <div className="header">
+            <NavLink to="/">
+                Home
+            </NavLink>
+            <NavLink to="/users">
+                Users
+            </NavLink>
+            <NavLink to="/posts">
+                Posts
+            </NavLink>
+            <NavLink to="/album">
+                Album
+            </NavLink>
+            <NavLink to="/login">
+                Login
+            </NavLink>
         </div>
     );
 };
